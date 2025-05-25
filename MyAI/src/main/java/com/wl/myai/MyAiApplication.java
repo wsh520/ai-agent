592b464d92456e3721172e592ff2fc9412ff8f1b
wl <wl16675181281@163.com>
2025-05-25 16:39:46 +0800

    spring boot 引入langchain4j,完成模型接入测试

---
+++ MyAI/src/main/java/com/wl/myai/MyAiApplication.java
@@ -0,0 +1,13 @@
+package com.wl.myai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAiApplication.class, args);
+    }
+
+}
---
+++ MyAI/src/test/java/com/wl/myai/MyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wl.myai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
