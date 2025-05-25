61b69c03ea18b1b87d58acd1a7bc2cc06ba9ade3
wl <wl16675181281@163.com>
2025-05-25 17:09:32 +0800

    接入 阿里云百炼 通义大模型

--- MyAI/src/test/java/com/wl/myai/LLMTest.java
+++ MyAI/src/test/java/com/wl/myai/LLMTest.java
@@ -1,9 +1,14 @@
 package com.wl.myai;
 
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.community.model.dashscope.WanxImageModel;
+import dev.langchain4j.data.image.Image;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -53,5 +58,37 @@ public class LLMTest {
         String answer = ollamaChatModel.chat("你好");
         //输出结果
         System.out.println(answer);
+    }
+
+    /**
+     * 通义千问大模型
+     */
+    @Autowired
+    private QwenChatModel qwenChatModel;
+
+    @Test
+    public void testDashScopeQwen() {
+        //向模型提问
+        String answer = qwenChatModel.chat("你好");
+        //输出结果
+        System.out.println(answer);
+    }
+
+
+    @Value("${langchain4j.community.dashscope.chat-model.api-key}")
+    private String apiKey;
+    @Test
+    public void testDashScopeWanx() {
+        WanxImageModel wanxImageModel = WanxImageModel.builder()
+                .modelName("wanx2.1-t2i-plus")
+                .apiKey(apiKey)
+                .build();
+        Response<Image> response = wanxImageModel.generate("""
+                奇幻森林精灵：在一片弥漫着轻柔薄雾的
+                                古老森林深处，阳光透过茂密枝叶洒下金色光斑。一位身材娇小、长着透明薄翼的精灵少女站在一朵硕大的蘑菇上。她
+                                有着海藻般的绿色长发，发间点缀着蓝色的小花，皮肤泛着珍珠般的微光。身上穿着由翠绿树叶和白色藤蔓编织而成的
+                                连衣裙，手中捧着一颗散发着柔和光芒的水晶球，周围环绕着五彩斑斓的蝴蝶，脚下是铺满苔藓的地面，蘑菇和蕨类植
+                                物丛生，营造出神秘而梦幻的氛围。""");
+                System.out.println(response.content().url());
     }
 }
